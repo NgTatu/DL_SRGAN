7f15e6e6929e4714aed16004e4dd384bf553abd1
NgTatu <taituan.ng@gmail.com>
2020-11-30 17:50:53 +0700

    add config file

---
+++ CONFIG.py
@@ -0,0 +1,10 @@
+HR_CROP_SIZE = 300
+LR_CROP_SIZE = HR_CROP_SIZE/4
+UPSCALE_FACTOR = 4
+NUM_EPOCHS = 20
+
+TRAIN_HR_DIR = '/home/tuan/Documents/VBDI/SRGAN/data/DIV2K_train_HR'
+TRAIN_LR_DIR = '/home/tuan/Documents/VBDI/SRGAN/data/DIV2K_train_LR_bicubic_X4/DIV2K_train_LR_bicubic/X4'
+
+VAL_HR_DIR = '/home/tuan/Documents/VBDI/SRGAN/data/DIV2K_valid_HR'
+VAL_LR_DIR = '/home/tuan/Documents/VBDI/SRGAN/data/DIV2K_valid_LR_bicubic_X4/DIV2K_valid_LR_bicubic/X4'
