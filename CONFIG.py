e0b6191064e2bbe9dd1869f203a59048aa204bf6
NgTatu <taituan.ng@gmail.com>
2020-12-02 17:29:53 +0700

    Add dir for Colab use

--- CONFIG.py
+++ CONFIG.py
@@ -9,3 +9,10 @@ TRAIN_LR_DIR = '/home/tuan/Documents/VBDI/SRGAN/data/DIV2K_train_LR_bicubic_X4/D
 
 VAL_HR_DIR = '/home/tuan/Documents/VBDI/SRGAN/data/DIV2K_valid_HR'
 VAL_LR_DIR = '/home/tuan/Documents/VBDI/SRGAN/data/DIV2K_valid_LR_bicubic_X4/DIV2K_valid_LR_bicubic/X4'
+
+### USE THIS FOR COLAB ####
+# TRAIN_HR_DIR = '/content/drive/My Drive/srgan/srgan_tensorlayer/data/DIV2K_train_HR'
+# TRAIN_LR_DIR = '/content/drive/My Drive/srgan/srgan_tensorlayer/data/DIV2K_train_LR_bicubic_X4/DIV2K_train_LR_bicubic/X4'
+#
+# VAL_HR_DIR = '/content/drive/My Drive/srgan/srgan_tensorlayer/data/DIV2K_valid_HR'
+# VAL_LR_DIR = '/content/drive/My Drive/srgan/srgan_tensorlayer/data/DIV2K_valid_LR_bicubic_X4/DIV2K_valid_LR_bicubic/X4'
