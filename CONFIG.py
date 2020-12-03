ea043851fa809fbb500f824310d556b65ad5a263
NgTatu <taituan.ng@gmail.com>
2020-12-03 22:34:52 +0700

    Change dir for Colab

--- CONFIG.py
+++ CONFIG.py
@@ -10,15 +10,15 @@ PATH_MODEL_G = MODEL_CHECKPOINT_PATH + 'G.pt'
 PATH_MODEL_D = MODEL_CHECKPOINT_PATH + 'D.pt'
 SR_PATH = '/content/drive/My Drive/srgan/SR_images/'
 
-TRAIN_HR_DIR = '/home/tuan/Documents/VBDI/SRGAN/data/DIV2K_train_HR'
-TRAIN_LR_DIR = '/home/tuan/Documents/VBDI/SRGAN/data/DIV2K_train_LR_bicubic_X4/DIV2K_train_LR_bicubic/X4'
-
-VAL_HR_DIR = '/home/tuan/Documents/VBDI/SRGAN/data/DIV2K_valid_HR'
-VAL_LR_DIR = '/home/tuan/Documents/VBDI/SRGAN/data/DIV2K_valid_LR_bicubic_X4/DIV2K_valid_LR_bicubic/X4'
+# TRAIN_HR_DIR = '/home/tuan/Documents/VBDI/SRGAN/data/DIV2K_train_HR'
+# TRAIN_LR_DIR = '/home/tuan/Documents/VBDI/SRGAN/data/DIV2K_train_LR_bicubic_X4/DIV2K_train_LR_bicubic/X4'
+#
+# VAL_HR_DIR = '/home/tuan/Documents/VBDI/SRGAN/data/DIV2K_valid_HR'
+# VAL_LR_DIR = '/home/tuan/Documents/VBDI/SRGAN/data/DIV2K_valid_LR_bicubic_X4/DIV2K_valid_LR_bicubic/X4'
 
 ### USE THIS FOR COLAB ####
-# TRAIN_HR_DIR = '/content/drive/My Drive/srgan/srgan_tensorlayer/data/DIV2K_train_HR'
-# TRAIN_LR_DIR = '/content/drive/My Drive/srgan/srgan_tensorlayer/data/DIV2K_train_LR_bicubic_X4/DIV2K_train_LR_bicubic/X4'
-#
-# VAL_HR_DIR = '/content/drive/My Drive/srgan/srgan_tensorlayer/data/DIV2K_valid_HR'
-# VAL_LR_DIR = '/content/drive/My Drive/srgan/srgan_tensorlayer/data/DIV2K_valid_LR_bicubic_X4/DIV2K_valid_LR_bicubic/X4'
+TRAIN_HR_DIR = '/content/drive/My Drive/srgan/srgan_tensorlayer/data/DIV2K_train_HR'
+TRAIN_LR_DIR = '/content/drive/My Drive/srgan/srgan_tensorlayer/data/DIV2K_train_LR_bicubic_X4/DIV2K_train_LR_bicubic/X4'
+
+VAL_HR_DIR = '/content/drive/My Drive/srgan/srgan_tensorlayer/data/DIV2K_valid_HR'
+VAL_LR_DIR = '/content/drive/My Drive/srgan/srgan_tensorlayer/data/DIV2K_valid_LR_bicubic_X4/DIV2K_valid_LR_bicubic/X4'
